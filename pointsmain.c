001d57b3a9850ca25e30a93f09ddab03bb6e0682
Zaid NAZAM <znazam@c4r16s10.wethinkcode.co.za>
2019-07-08 16:14:25 +0200

    edited

--- pointsmain.c
+++ pointsmain.c
@@ -1,41 +1,128 @@
 #include <mlx.h>
 #include <stdlib.h>
 # define PIXEL mlx_pixel_put(mlx, win, x, y, 0x0ffffff)
-# define ABS abs()
-int main()
+# define ABS(X) (X < 0 ? -X : X) 
+
+typedef struct  s_pixel
 {
-    void *mlx = mlx_init();
-    void *win = mlx_new_window(mlx, 500, 500, "test");
-    int r0;
-    int r1;
-    int d0;
-    int d1;
-    int dr = r0 - r1;
-    int dd = d0 - d1; 
-    int steps;
-    int xi;
-    int yi;
-    int v = 0;
+    int x;
+    int y;
+}               t_pixel;
+
+typedef struct  s_env
+{
+    void *mlx_ptr;
+    void *win_ptr;
+}               t_env;
+
+#include <stdio.h>
+
+void draw_box(t_env *env, t_pixel a, t_pixel b)
+{
     int x;
     int y;
 
-    r0 = 250;
-    r1 = 300;
-    d0 = 250;
-    d1 = 300;
-    if (ABS(dr) > ABS(dd))
-        steps = ABS(dr);
-    else
-        steps = ABS(dd);
-    xi = dr / (float) steps;
-    yi = dd / (float) steps;
-    if (v < steps)
+    if (a.x > b.x)
     {
-        x = x + xi;
-        y = y + yi;
-        PIXEL;
+        int temp = a.x;
+        a.x = b.x;
+        b.x = temp;
     }
+    if (a.y > b.y)
+    {
+        int temp = a.y;
+        a.y = b.y;
+        b.y = temp;
+    }
+    y = a.y;
+    while (y < b.y)
+    {
+        x = a.x;
+        while (x < b.x)
+        {
+            mlx_pixel_put(env->mlx_ptr, env->win_ptr, x, y, 0xffffff);
+            x++;
+        }
+        y++;
+    }
+}
+void draw_line_y(t_env *env, t_pixel a, t_pixel b)
+{
+    double x;
+    double y;
 
-    mlx_loop(mlx);
+    int dx;
+    int dy;
+
+    double gradient;
+
+    dx = b.x - a.x;
+    dy = b.y - a.y;
+
+    gradient = (double)dx / (double)dy;
+    y = 0;
+    x = 0;
+    while (y < ABS(dy))
+    {
+        mlx_pixel_put(env->mlx_ptr, env->win_ptr, a.x + y * gradient, a.y + y, 0xffffff);
+        y++;
+    }
+}
+
+void draw_line(t_env *env, t_pixel a, t_pixel b)
+{
+    double x;
+    double y;
+
+    int dx;
+    int dy;
+
+    double gradient;
+
+    dx = b.x - a.x;
+    dy = b.y - a.y;
+
+    gradient = (double)dy / (double)dx;
+    if (gradient > 1)
+        draw_line_y(env, a, b);
+    y = 0;
+    x = 0;
+    while (x < ABS(dx))
+    {
+        mlx_pixel_put(env->mlx_ptr, env->win_ptr, a.x + x, a.y + x * gradient, 0xffffff);
+        x++;
+    }
+}
+
+int fun(void *data)
+{
+    t_env * env = (t_env *)data;
+    t_pixel p;
+    p.x = 50;
+    p.y = 50;
+    t_pixel p1;
+    p1.x = 100;
+    p1.y = 300;
+
+    draw_line(env, p, p1);
+
+    return 0;
+}
+
+int exitfun(void *data)
+{
+    exit(0);
+    return 0;
+}
+
+int main()
+{
+    t_env env;
+    env.mlx_ptr = mlx_init();
+    env.win_ptr = mlx_new_window(env.mlx_ptr, 500, 500, "test");
+
+    mlx_loop_hook(env.mlx_ptr, fun, &env);
+    mlx_hook(env.win_ptr, 17, 0L, exitfun, &env);
+    mlx_loop(env.mlx_ptr);
     return (0);
 }
